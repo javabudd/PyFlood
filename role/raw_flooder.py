794886145fccff9ab7f9532a1f69836d29a43b5e
andy obrien <andy@1bg.com>
2016-08-09 18:39:12 -0600

    commenting out receive block

--- role/raw_flooder.py
+++ role/raw_flooder.py
@@ -118,14 +118,14 @@ class RAWFlooder(flooder.Flooder):
 			while True:
 				try:
 					s.send(packet)
-					try:
-						s.settimeout(5)
-						resp = s.recv(1024)
-					except socket_error as e:
-						print(e)
-						s.close()
-						s = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)
-						s.connect((self.ip, int(self.port)))
+					# try:
+					# 	s.settimeout(5)
+					# 	resp = s.recv(1024)
+					# except socket_error as e:
+					# 	print(e)
+					# 	s.close()
+					# 	s = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)
+					# 	s.connect((self.ip, int(self.port)))
 
 				except socket_error:
 					try:
